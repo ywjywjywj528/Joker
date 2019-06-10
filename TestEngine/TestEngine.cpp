fbfada66f87f8e5fea443fd831d895c2e867b06d
ywj528 <15068832221@163.com>
2019-06-10 20:39:53 +0800

    Add files via upload

---
+++ TestEngine/TestEngine.cpp
@@ -0,0 +1,12 @@
+// TestEngine.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "Director.h"
+using namespace Joker;
+int main()
+{
+	Scene* mainSecne = Director::getInstance()->CreateScene();
+    return 0;
+}
+
