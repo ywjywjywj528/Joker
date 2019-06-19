b498bf6180f39f3422b216eeb47a210730c84120
ywwj528 <15068832221@163.com>
2019-06-19 18:20:21 +0800

    upload

--- Joke_Engine/Include/Mesh.h
+++ Joke_Engine/Include/Mesh.h
@@ -1,8 +1,19 @@
 #pragma once
+#include "OGLHeader.h"
 namespace Joker
 {
 	class Mesh
 	{
+	public:
+		enum PrimitiveType
+		{
+			TRIANGLES = GL_TRIANGLES,
+			TRIANGLE_STRIP = GL_TRIANGLE_STRIP,
+			LINES = GL_LINES,
+			LINE_STRIP = GL_LINE_STRIP,
+			POINTS = GL_POINTS
+		};
+
 	public:
 		static Mesh* CreateFromFile(const char* _filePath);
 
